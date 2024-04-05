f2d2fc83fc0b4b7934450967f10a58692bfbaf3a
rouman1201082 <chernovrn@gmail.com>
2024-04-05 20:07:29 +0400

    05.04.24

--- cart.js
+++ cart.js
@@ -1,23 +1,38 @@
 'use strict';
 
 
+// Задание 1
+// Продолжим работу с cart.js
+// Обнаружена уязвимость в нашем функционале
+/* Если после добавления последнего товара
+присвоить к totalPrice любое значение,*/
+// например
+// cart.totalPrice = 10;
+// то при выводе print() общая стоимость корзины будет равна 10
+/* Чтобы это предотвратить, необходимо свойство totalPrice сделать геттером
+ который будет возвращать результат вызова метода calculateItemPrice */
+// метод getTotalPrice больше не нужен
+/* calculateItemPrice переделать таким образом, чтобы сумму он возвращал,
+ а не записывал в свойство totalPrice */
+// Вызов метода calculateItemPrice оставить только у геттера totalPrice
+
 const cart = {
   items: [],
   count: 0,
-  totalPrice: 0,
+
+  /* Чтобы это предотвратить, необходимо свойство totalPrice сделать геттером
+ который будет возвращать результат вызова метода calculateItemPrice */
 
   get totalPrice() {
     return this.calculateItemPrice();
   },
 
   add(name, price, amount = 1) {
-
     const goods = {
       name,
       price,
       amount,
     };
-
     this.items.push(goods);
     this.increaseCount(amount);
   },
@@ -25,10 +40,10 @@ const cart = {
   increaseCount(num) {
     this.count += num;
   },
-
+  /* calculateItemPrice переделать таким образом,
+чтобы сумму он возвращал, а не записывал в свойство totalPrice*/
   calculateItemPrice() {
-    const check = this.items.reduce((sum, item) => sum + item.price * item.amount, 0);
-    return check;
+    this.items.reduce((sum, item) => sum + item.price * item.amount, 0);
   },
 
   clear() {
@@ -38,16 +53,16 @@ const cart = {
 
   print() {
     console.log(JSON.stringify(cart.items));
-    console.log(`TotalPrice: ${this.totalPrice}`);
+    console.log(`TotalPrice: ${this.totalPrice}`); // 25060
   },
 };
 
-cart.add('часы', 5, 20);
+cart.add('часы', 20, 25);
 cart.add('коньки', 23, 10);
-cart.add('Мяч', 15, 12);
+cart.add('Мячь', 15, 12);
+cart.add('лыжи', 345, 70);
+/* Cannot set property totalPrice of #<Object> which has only a getter
+(Не удается задать суммарное значение свойства #, которое имеет только фильтр)*/
 
 cart.print();
-
 console.log(cart);
-
-
---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// import { listIPv4 } from 'modules/ipv4.js';
+
+function getUniqueIpAddresses() {
+  let array = [];
+  let num = 0;
+  const UniqueIpAddresses = new Set(listIPv4);
+  array = [...new Set(UniqueIpAddresses)];
+  return num = array.length
+}
+
+// let res = getUniqueIpAddresses(listIPv4);
+
+// console.log('res:', res);
